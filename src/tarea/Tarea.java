1f8cf88a7acc2395f12add87a59a40d5051988e2
Oscar Lovato <Oscar Lovato@LAPTOP-HN0CBI2R>
2018-03-14 11:56:51 -0600

    Primera versión de datos generales

---
+++ src/tarea/Tarea.java
@@ -0,0 +1,16 @@
+package tarea;
+
+/**
+ * @author Oscar Lovato
+ */
+public class Tarea {
+
+  
+    public static void main(String[] args) {
+        String Nombre="Oscar Edenilson", Apellido="Lovato Quintanilla", Carné="00127417", estado="Soltero",fecha="31 de octubre de 1998";
+        int Numtel=23007713;
+        System.out.println("Nombre: " + Nombre + "\n" +"Apellidos: " + Apellido + "\n" + "Carné: " + Carné + "\n" + "Estado Civil: " + estado + "\n" + 
+              "Fecha de nacimiento: " + fecha + "\n" + "Numero de telefono: " + Numtel);
+    }
+    
+}
