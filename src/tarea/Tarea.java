5924167f0e26c4a8d32375366dc5542ad2ca60b3
Oscar Lovato <Oscar Lovato@LAPTOP-HN0CBI2R>
2018-03-14 12:02:31 -0600

    Con espacios

--- src/tarea/Tarea.java
+++ src/tarea/Tarea.java
@@ -9,7 +9,7 @@ public class Tarea {
     public static void main(String[] args) {
         String Nombre="Oscar Edenilson", Apellido="Lovato Quintanilla", Carné="00127417", estado="Soltero",fecha="31 de octubre de 1998";
         int Numtel=23007713;
-        System.out.println("Nombre: " + Nombre + "\n" +"Apellidos: " + Apellido + "\n" + "Carné: " + Carné + "\n" + "Estado Civil: " + estado + "\n" + 
+        System.out.println("Nombre : " + Nombre + "\n" +"Apellidos: " + Apellido + "\n" + "Carné: " + Carné + "\n" + "Estado Civil: " + estado + "\n" + 
               "Fecha de nacimiento: " + fecha + "\n" + "Numero de telefono: " + Numtel);
     }
     
